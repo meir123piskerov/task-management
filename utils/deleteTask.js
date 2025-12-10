c63479658d4334d045958cb0bab5663b74554c15
meir_p <meir12345p@gmail.com>
2025-12-10 16:56:55 +0200

    add else to deleteTask function

--- utils/deleteTask.js
+++ utils/deleteTask.js
@@ -2,6 +2,8 @@ export default function deleteTask(id) {
   for (let i = 0; i < listTask.length; i++) {
     if (i[id] === id) {
       listTask.splice(i, 1);
+    } else {
+      console.log("id not in tasks");
     }
   }
 }
