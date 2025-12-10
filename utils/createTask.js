a1d012f52b620ea194cf9cae62d421ebc3f3ffe4
meir_p <meir12345p@gmail.com>
2025-12-10 16:51:30 +0200

    fix create function: get another var as status

--- utils/createTask.js
+++ utils/createTask.js
@@ -1,12 +1,12 @@
 import { nanoid } from "nanoid";
-export default function create(details) {
+export default function create(details, Status) {
   let date = new Date().toLocaleDateString();
   let id = nanoid();
   let newTask = {
     id: id,
     details: details,
     date: date,
-    Status: "new",
+    Status: Status,
   };
   taskList.push(newTask);
 }
